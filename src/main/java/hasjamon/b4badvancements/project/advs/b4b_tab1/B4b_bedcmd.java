942f7d352e832c4fe4fe9632b8a31c991be48e85
Has Jam On <82700061+HasJamOn@users.noreply.github.com>
2025-11-25 11:29:09 +0800

    Implemented Emergency Bed

--- src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab1/B4b_bedcmd.java
+++ src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab1/B4b_bedcmd.java
@@ -5,8 +5,11 @@ import hasjamon.b4badvancements.project.advs.AdvancementTabNamespaces;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementDisplay;
 import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import org.bukkit.Material;
+import org.bukkit.Location;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class B4b_bedcmd extends BaseAdvancement  {
 
@@ -14,6 +17,16 @@ public class B4b_bedcmd extends BaseAdvancement  {
 
 
   public B4b_bedcmd(Advancement parent) {
-    super(KEY.getKey(), new AdvancementDisplay(Material.WHITE_BED, "(DISABLED)Emergency Bed", AdvancementFrameType.TASK, true, true, 3f, 13f, "Write '/bed' in Chat for a free bed, available once a day"), parent, 1);
+    super(KEY.getKey(), new AdvancementDisplay(Material.WHITE_BED, "Emergency Bed", AdvancementFrameType.TASK, true, true, 3f, 13f, "Respawn without a bed set to get a free bed; available once a day"), parent, 1);
+
+    registerEvent(PlayerRespawnEvent.class, (e) -> {
+      Player p = e.getPlayer();
+      Location bedSpawn = p.getBedSpawnLocation();
+
+      // Check if player doesn't have a bed spawn location set
+      if (bedSpawn == null && !e.isBedSpawn()) {
+        incrementProgression(p);
+      }
+    });
   }
 }
