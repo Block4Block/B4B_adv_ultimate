bc813f1e9a04bf076aa63b632c167172982b7384
Has Jam On <82700061+HasJamOn@users.noreply.github.com>
2025-11-25 12:14:57 +0800

    Implemented Tropical Caviar

--- src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab1/B4b_fishgetspawnegg.java
+++ src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab1/B4b_fishgetspawnegg.java
@@ -7,6 +7,10 @@ import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import org.bukkit.Material;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerFishEvent;
 
 public class B4b_fishgetspawnegg extends BaseAdvancement  {
 
@@ -14,6 +18,19 @@ public class B4b_fishgetspawnegg extends BaseAdvancement  {
 
 
   public B4b_fishgetspawnegg(Advancement parent) {
-    super(KEY.getKey(), new AdvancementDisplay(Material.TROPICAL_FISH_SPAWN_EGG, "(DISABLED)Tropical Fishing", AdvancementFrameType.TASK, true, true, 3f, 0f , "Catch a Tropical Fish Spawn Egg while Fishing."), parent, 1);
+    super(KEY.getKey(), new AdvancementDisplay(Material.TROPICAL_FISH_SPAWN_EGG, "Tropical Caviar", AdvancementFrameType.TASK, true, true, 3f, 0f , "Catch a Tropical Fish Spawn Egg while Fishing."), parent, 1);
+
+    registerEvent(PlayerFishEvent.class, (e) -> {
+      if (e.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
+        if (e.getCaught() != null && e.getCaught().getType() == EntityType.ITEM) {
+          Material itemType = ((Item) e.getCaught()).getItemStack().getType();
+
+          if (itemType == Material.TROPICAL_FISH_SPAWN_EGG) {
+            Player p = e.getPlayer();
+            incrementProgression(p);
+          }
+        }
+      }
+    });
   }
 }
