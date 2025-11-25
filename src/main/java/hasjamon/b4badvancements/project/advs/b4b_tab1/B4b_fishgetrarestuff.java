368d4764c4401cf584ca0f14495df171a7af078c
Has Jam On <82700061+HasJamOn@users.noreply.github.com>
2025-11-25 12:12:51 +0800

    Implemented Exotic Fishing

--- src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab1/B4b_fishgetrarestuff.java
+++ src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab1/B4b_fishgetrarestuff.java
@@ -7,6 +7,10 @@ import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import org.bukkit.Material;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerFishEvent;
 
 public class B4b_fishgetrarestuff extends BaseAdvancement  {
 
@@ -14,6 +18,19 @@ public class B4b_fishgetrarestuff extends BaseAdvancement  {
 
 
   public B4b_fishgetrarestuff(Advancement parent) {
-    super(KEY.getKey(), new AdvancementDisplay(Material.NAUTILUS_SHELL, "(DISABLED)Exotic Fishing", AdvancementFrameType.TASK, true, true, 4f, 0f , "Catch a rare fishing reward."), parent, 1);
+    super(KEY.getKey(), new AdvancementDisplay(Material.NAUTILUS_SHELL, "Exotic Fishing", AdvancementFrameType.TASK, true, true, 4f, 0f , "Catch a rare fishing reward."), parent, 1);
+
+    registerEvent(PlayerFishEvent.class, (e) -> {
+      if (e.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
+        if (e.getCaught() != null && e.getCaught().getType() == EntityType.ITEM) {
+          Material itemType = ((Item) e.getCaught()).getItemStack().getType();
+
+          if (itemType == Material.NAME_TAG || itemType == Material.NAUTILUS_SHELL) {
+            Player p = e.getPlayer();
+            incrementProgression(p);
+          }
+        }
+      }
+    });
   }
 }
