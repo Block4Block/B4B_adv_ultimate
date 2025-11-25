152c4c2697eb7c91da07cc6e9d53567ed14db60d
Has Jam On <82700061+HasJamOn@users.noreply.github.com>
2025-11-25 12:21:13 +0800

    Implemented Pointy Trident

--- src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab1/B4b_hitdripstonewithtrident.java
+++ src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab1/B4b_hitdripstonewithtrident.java
@@ -7,6 +7,10 @@ import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import org.bukkit.Material;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Trident;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.block.Block;
 
 public class B4b_hitdripstonewithtrident extends BaseAdvancement  {
 
@@ -14,6 +18,24 @@ public class B4b_hitdripstonewithtrident extends BaseAdvancement  {
 
 
   public B4b_hitdripstonewithtrident(Advancement parent) {
-    super(KEY.getKey(), new AdvancementDisplay(Material.POINTED_DRIPSTONE, "(DISABLED)Pointy Trident", AdvancementFrameType.TASK, true, true, 4f, 12f, "You broke pointed dripstone with a thrown trident. Nice!"), parent, 1);
+    super(KEY.getKey(), new AdvancementDisplay(Material.POINTED_DRIPSTONE, "Pointy Trident", AdvancementFrameType.TASK, true, true, 4f, 12f, "You broke pointed dripstone with a thrown trident. Nice!"), parent, 1);
+
+    registerEvent(ProjectileHitEvent.class, (e) -> {
+      // Check if the projectile is a trident
+      if (e.getEntity() instanceof Trident) {
+        Trident trident = (Trident) e.getEntity();
+
+        // Check if the trident was thrown by a player
+        if (trident.getShooter() instanceof Player) {
+          Player p = (Player) trident.getShooter();
+
+          // Check if the trident hit a block
+          Block hitBlock = e.getHitBlock();
+          if (hitBlock != null && hitBlock.getType() == Material.POINTED_DRIPSTONE) {
+            incrementProgression(p);
+          }
+        }
+      }
+    });
   }
 }
