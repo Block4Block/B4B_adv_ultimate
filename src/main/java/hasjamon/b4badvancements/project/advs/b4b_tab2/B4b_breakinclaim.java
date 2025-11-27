ef1f2406e2ef8f63f3e9ee65175accf0606c3c79
Has Jam On <82700061+HasJamOn@users.noreply.github.com>
2025-11-27 13:03:46 +0800

    Implemented Reclaimed Rights

--- src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab2/B4b_breakinclaim.java
+++ src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab2/B4b_breakinclaim.java
@@ -7,6 +7,8 @@ import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import org.bukkit.Material;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import org.bukkit.entity.Player;
+import hasjamon.block4block.events.BlockBreakInClaimEvent;
 
 public class B4b_breakinclaim extends BaseAdvancement  {
 
@@ -14,6 +16,15 @@ public class B4b_breakinclaim extends BaseAdvancement  {
 
 
   public B4b_breakinclaim(Advancement parent) {
-    super(KEY.getKey(), new AdvancementDisplay(Material.RED_BANNER, "(DISABLED)Reclaimed Rights", AdvancementFrameType.TASK, true, true, 6f, 1f, "Some blocks are free to break, if you have a claim. In this case, you do."), parent, 1);
+    super(KEY.getKey(), new AdvancementDisplay(Material.RED_BANNER, "Reclaimed Rights", AdvancementFrameType.TASK, true, true, 6f, 1f, "Some blocks are free to break, if you have a claim. In this case, you do."), parent, 1);
+
+    registerEvent(BlockBreakInClaimEvent.class, (e) -> {
+      Player p = e.player;
+
+      // Only grant if the break was successful (claim-specific free break)
+      if (e.success) {
+        incrementProgression(p);
+      }
+    });
   }
 }
