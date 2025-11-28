d10d869490fc1a4de5e919636eb8baeb0283e241
Has Jam On <82700061+HasJamOn@users.noreply.github.com>
2025-11-28 10:40:06 +0800

    implemented Lectern's Claim List

--- src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab2/B4b_claimchunk.java
+++ src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab2/B4b_claimchunk.java
@@ -7,6 +7,8 @@ import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import org.bukkit.Material;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import org.bukkit.entity.Player;
+import hasjamon.block4block.events.ClaimBookPlacedEvent;
 
 public class B4b_claimchunk extends BaseAdvancement  {
 
@@ -14,6 +16,15 @@ public class B4b_claimchunk extends BaseAdvancement  {
 
 
   public B4b_claimchunk(Advancement parent) {
-    super(KEY.getKey(), new AdvancementDisplay(Material.LECTERN, "(DISABLED)Lectern's Claim List", AdvancementFrameType.TASK, true, true, 5f, 0f, "Create a claim by placing a claim book on a lectern."), parent, 1);
+    super(KEY.getKey(), new AdvancementDisplay(Material.LECTERN, "Lectern's Claim List", AdvancementFrameType.TASK, true, true, 5f, 0f, "Create a claim by placing a claim book on a lectern."), parent, 1);
+
+    registerEvent(ClaimBookPlacedEvent.class, (e) -> {
+      Player p = e.player;
+
+      // Grant advancement when player places a claim book in a lectern, and they're a member
+      if (e.isMember) {
+        incrementProgression(p);
+      }
+    });
   }
 }
