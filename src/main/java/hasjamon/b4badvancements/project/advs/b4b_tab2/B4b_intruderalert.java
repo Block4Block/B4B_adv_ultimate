e1f2cebbc67bee43e1a7d400c0670e0c89b78e9f
Has Jam On <82700061+HasJamOn@users.noreply.github.com>
2025-11-29 20:53:58 +0800

    Implemented Intruder Alert

--- src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab2/B4b_intruderalert.java
+++ src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab2/B4b_intruderalert.java
@@ -7,6 +7,8 @@ import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import org.bukkit.Material;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import org.bukkit.entity.Player;
+import hasjamon.block4block.events.IntruderEnteredClaimEvent;
 
 public class B4b_intruderalert extends BaseAdvancement  {
 
@@ -14,6 +16,11 @@ public class B4b_intruderalert extends BaseAdvancement  {
 
 
   public B4b_intruderalert(Advancement parent) {
-    super(KEY.getKey(), new AdvancementDisplay(Material.MUSIC_DISC_STRAD, "(DISABLED)Intruder Alert", AdvancementFrameType.TASK, true, true, 6f, 7f, "Be alerted of an intruder in one of your claims."), parent, 1);
+    super(KEY.getKey(), new AdvancementDisplay(Material.MUSIC_DISC_STRAD, "Intruder Alert", AdvancementFrameType.TASK, true, true, 6f, 7f, "Be alerted of an intruder in one of your claims."), parent, 1);
+
+    registerEvent(IntruderEnteredClaimEvent.class, (e) -> {
+      Player p = e.claimOwner;
+      incrementProgression(p);
+    });
   }
 }
