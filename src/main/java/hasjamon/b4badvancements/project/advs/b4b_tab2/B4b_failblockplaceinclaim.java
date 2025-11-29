856d9bda82a917e6940486ba8a9f13d78988d15f
Has Jam On <82700061+HasJamOn@users.noreply.github.com>
2025-11-29 20:51:51 +0800

    Implemented Access Denied

--- src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab2/B4b_failblockplaceinclaim.java
+++ src/main/java/hasjamon/b4badvancements/project/advs/b4b_tab2/B4b_failblockplaceinclaim.java
@@ -7,6 +7,8 @@ import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import org.bukkit.Material;
 import com.fren_gor.ultimateAdvancementAPI.advancement.display.AdvancementFrameType;
 import com.fren_gor.ultimateAdvancementAPI.advancement.Advancement;
+import org.bukkit.entity.Player;
+import hasjamon.block4block.events.BlockPlaceInClaimEvent;
 
 public class B4b_failblockplaceinclaim extends BaseAdvancement  {
 
@@ -14,6 +16,15 @@ public class B4b_failblockplaceinclaim extends BaseAdvancement  {
 
 
   public B4b_failblockplaceinclaim(Advancement parent) {
-    super(KEY.getKey(), new AdvancementDisplay(Material.WARPED_SIGN, "(DISABLED)Access Denied", AdvancementFrameType.TASK, true, true, 6f, 4f, "Attempt to place a block inside a claim you're intruding on."), parent, 1);
+    super(KEY.getKey(), new AdvancementDisplay(Material.WARPED_SIGN, "Access Denied", AdvancementFrameType.TASK, true, true, 6f, 4f, "Attempt to place a block inside a claim you're intruding on."), parent, 1);
+
+    registerEvent(BlockPlaceInClaimEvent.class, (e) -> {
+      Player p = e.player;
+
+      // Only grant if the placement failed (not their claim)
+      if (!e.success) {
+        incrementProgression(p);
+      }
+    });
   }
 }
